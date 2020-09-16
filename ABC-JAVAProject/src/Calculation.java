16f3fd2a8f698e993a2df87f3b50a9cbead73e6e
ramkumarsjava <ramkumar.k999@gmail.com>
2020-09-16 09:40:02 +0530

    Commit by Ram (add method)

--- ABC-JAVAProject/src/Calculation.java
+++ ABC-JAVAProject/src/Calculation.java
@@ -1,9 +1,17 @@
 
 public class Calculation {
 
+	public void add()
+	{
+		System.out.println("Hello World");
+	}
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
